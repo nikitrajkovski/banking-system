60dd1783d87e4b681de23a6b19e3cfcd3ae0c21b
Nikola Trajkovski <ntrajkovski5@gmail.com>
2026-05-16 20:03:10 +0200

    initial commit

---
+++ banking-system-java/src/main/java/finki/diplomska/banking_system/BankingSystemApplication.java
@@ -0,0 +1,13 @@
+package finki.diplomska.banking_system;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BankingSystemApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BankingSystemApplication.class, args);
+	}
+
+}
---
+++ banking-system-java/src/test/java/finki/diplomska/banking_system/BankingSystemApplicationTests.java
@@ -0,0 +1,13 @@
+package finki.diplomska.banking_system;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BankingSystemApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
